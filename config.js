afb2f95f595dd8687b7f0259d3c66ea304232438
mGresset <matthieu.gresset@orange.fr>
2026-07-24 23:07:53 +0200

    Initial project structure

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+
+    appName: "Shuffle+",
+
+    version: "0.1.0",
+
+    clientId: "34ea30d5c73e4c4bb012160e17c18a32",
+
+    redirectUri: "https://mgresset.github.io/spotify-callback/",
+
+    scopes: [
+        "user-library-read",
+        "playlist-read-private",
+        "playlist-read-collaborative",
+        "user-read-playback-state",
+        "user-modify-playback-state"
+    ]
+
+};
